c3e24d444c1adcafc50202a1b76ab722dd5a2e20
Chris <csleary@gmail.com>
2017-06-28 13:51:11 +0100

    More nodes, copy tweaks, general tidy and commenting.

--- app.rb
+++ app.rb
@@ -8,11 +8,6 @@ require 'net/http'
 require 'rqrcode'
 require 'sinatra'
 
-helpers do
-  include Rack::Utils
-  alias_method :h, :escape_html
-end
-
 set :root, File.dirname(__FILE__)
 set :price, 40
 
@@ -20,6 +15,8 @@ if settings.production?
   set :payment_address, 'NBCR2G-JL7VJF-3FKVI6-6SMZCG-4YBC6H-3BM2A6-LLTM'
   set :nodes, [
     '85.25.36.97:7890',
+    '62.75.163.236:7890',
+    '199.217.112.135:7890',
     '108.61.182.27:7890',
     '108.61.168.86:7890',
     '104.238.161.61:7890',
@@ -38,6 +35,7 @@ else
 end
 
 get '/' do
+  # Fetch current xem prices via exchange APIs.
   xem_price_btc = Net::HTTP.get_response(
     URI('https://bittrex.com/api/v1.1/public/getticker?market=btc-xem')
   )
@@ -55,7 +53,7 @@ get '/' do
     if xbt_price_usd.is_a? Net::HTTPSuccess
       JSON.parse(xbt_price_usd.body)['result']['XXBTZUSD']['c'][0].to_f
     else
-      2200.00
+      2500.00
     end
 
   @xem_price_usd = (@xbt_price_last * 10**-8) * @xem_price_satoshis
@@ -67,7 +65,7 @@ post '/' do
   @xem_price_usd = params[:xem_price_usd].to_f
   @usd_price = @xem_price_usd * settings.price
 
-  # Truncate the hash for cheaper tx fee.
+  # Calculate customer ID hash and truncate it for cheaper tx fee.
   @id_hash = Digest::SHA256.hexdigest(params[:user_email] +
   ENV['NEMP3_SECRET'])[0, 31]
   payment_data = {
@@ -80,6 +78,7 @@ post '/' do
     }
   }
 
+  # Present QR code and payment information.
   qrcode = RQRCode::QRCode.new(payment_data.to_json)
   @qr = qrcode.as_svg(
     offset: 0,
@@ -92,6 +91,7 @@ post '/' do
 end
 
 post '/download' do
+  # Connect to a node.
   node = ''
   @node_name = ''
 
@@ -100,21 +100,19 @@ post '/download' do
       node_info = Net::HTTP.get_response(
         URI("http://#{node_address}/node/info")
       )
-      if node_info.is_a? Net::HTTPSuccess
-        node = node_address
-        @node_name = JSON.parse(node_info.body)['identity']['name']
-        break
-      else
-        next
-      end
+      next unless node_info.is_a? Net::HTTPSuccess
+      node = node_address
+      @node_name = JSON.parse(node_info.body)['identity']['name']
+      break
     rescue
       next
     end
   end
 
-  # Search for txs in groups of 25
+  # Fetch transactions in groups of 25.
   parameters = ''
   data = []
+
   loop do
     transfers = Net::HTTP.get(
       URI("http://#{node}/account/transfers/incoming?address="\
@@ -128,6 +126,7 @@ post '/download' do
     data.concat latest_data
   end
 
+  # Search transactions for customer purchases.
   @id_hash = params[:id_hash]
   @encoded_message = @id_hash.unpack('H*')
   @search = data.find_all do |tx|
@@ -137,6 +136,7 @@ post '/download' do
   @paid = []
   @transaction = @search.count > 1 ? 'transactions' : 'transaction'
 
+  # Decide how to act depending on search results.
   if @search.empty?
     erb :tx_not_found
   else
@@ -158,21 +158,21 @@ end
 post '/:download_link' do
   signer = Aws::S3::Presigner.new
   url =
-  if settings.production?
-    signer.presigned_url(
-      :get_object,
-      bucket: 'nemp3',
-      key: 'Ochre - Beyond the Outer Loop.zip',
-      expires_in: 300
-    )
-  else
-    signer.presigned_url(
-      :get_object,
-      bucket: 'nemp3',
-      key: 'Empty.zip',
-      expires_in: 300
-    )
-  end
+    if settings.production?
+      signer.presigned_url(
+        :get_object,
+        bucket: 'nemp3',
+        key: 'Ochre - Beyond the Outer Loop.zip',
+        expires_in: 300
+      )
+    else
+      signer.presigned_url(
+        :get_object,
+        bucket: 'nemp3',
+        key: 'Empty.zip',
+        expires_in: 300
+      )
+    end
   redirect url
 end
 
